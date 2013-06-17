31c0081a279fa0c1e0ea6934c2802bebd2f6fe8d
Tim Welch <tim.j.welch@gmail.com>
2013-06-16 22:04:01 -0700

    Completed #2 - add id to admin display

--- reports/admin.py
+++ reports/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from models import *
 
 class ReportAdmin(admin.ModelAdmin):
-    list_display = ('imei', 'timestamp', 'ak_time', 'status', 'rep_type', 'loc', 'speed', 'course', 'voltage')
+    list_display = ('id', 'imei', 'timestamp', 'ak_time', 'status', 'rep_type', 'loc', 'speed', 'course', 'voltage')
 
 admin.site.register(Report, ReportAdmin)
