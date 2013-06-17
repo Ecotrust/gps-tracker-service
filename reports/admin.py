c25c246af13bcc81c2ec5264e81bef7e29b8f81c
Tim Welch <tim.j.welch@gmail.com>
2013-06-16 22:57:47 -0700

    Completed #5 - geoadmin with openstreetmap layer

--- reports/admin.py
+++ reports/admin.py
@@ -1,7 +1,7 @@
-from django.contrib import admin
+from django.contrib.gis import admin
 from models import *
 
-class ReportAdmin(admin.ModelAdmin):
+class ReportAdmin(admin.OSMGeoAdmin):
     list_display = ('id', 'imei', 'timestamp', 'ak_time', 'status', 'rep_type', 'loc', 'speed', 'course', 'voltage')
-
+    ordering = ('timestamp','id')
 admin.site.register(Report, ReportAdmin)
