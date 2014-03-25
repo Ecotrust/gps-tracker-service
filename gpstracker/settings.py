c842ff8cbe89c99d8ad627df5f70480cbbdad5fd
Edwin Knuth <edwin@pointnineseven.com>
2014-03-24 21:22:51 -0700

    turning on debug

--- gpstracker/settings.py
+++ gpstracker/settings.py
@@ -1,7 +1,7 @@
 # Django settings for gpslogger project.
 import os
 
-DEBUG = False
+DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
@@ -20,7 +20,7 @@ STATICFILES_DIRS = (
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
